9bfa1ee59e35a4abba33843f4376293912f19155
webDevSociapa <webdev@sociapa.com>
2024-12-13 11:57:57 +0530

    fixeesss

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,7 @@ import createNextIntlPlugin from 'next-intl/plugin';
 const withNextIntl = createNextIntlPlugin();
 
 const nextConfig = {
+  output: 'export', // Enable static export
   experimental: {
     turbo: {
       rules: {
