77d79d6721afbd68b122ee2b463fefdb03500bbc
webDevSociapa <webdev@sociapa.com>
2025-03-26 17:17:16 +0530

    fixesss

--- try.js
+++ try.js
@@ -11,8 +11,17 @@ const Data = [
   ];
   
   // Find the index of "Our Blog"
-  const index = Data.findIndex((item) => item.name === "investors");
-  console.log("index", index);
+  const index = Data.findIndex((item) => item.name === "Careers");
+  // console.log("index", index);
+
+  // const Data12 = [
+  //   {id:"1",name:"A"},
+  //   {id:"2",name:"B"},
+  //   {id:"3",name:"C"}
+  // ]
+
+  // const indexfind = Data12.findIndex((item)=> item.id === "1");
+  // console.log(indexfind);
   
   
   // Remove "Our Blog" if it exists
@@ -20,62 +29,98 @@ const Data = [
     Data.splice(index, 5); // Remove 1 element at the found index
   }
 
-  const newwwData = Data.map((item)=> item.url.split("/")[1]);
-  console.log("newwwData",newwwData);
 
-  console.log("newwData",newwwData);
+
+
+
+
+const array = [1, 2, 3, 4, 5];
+
+// Checks whether an element is even
+const even = (element) => element % 2 === 0;
+
+console.log(array.some(even));
+// Expected output: true
   
 
-//   split,splice,slice,forEach,map,
+//   const newwwData = Data.map((item)=> item.url.split("/")[1]);
+//   console.log("newwwData",newwwData);
 
-const arr = [4,6,3,6,6,43];
-let max = arr[0];
+//   console.log("newwData",newwwData);
+  
 
-for(let i =1; i<arr.length; i++){
-    if(arr[i]<max){
-        max = arr[i]
-    }
-}
+// //   split,splice,slice,forEach,map,
 
-const maxNum = Math.min(...arr);
-console.log("maxNum",maxNum);
+// const arr = [4,6,3,6,6,43];
+// let max = arr[0];
 
-const array = [3,4,1,7,9];
-const newArray = array.map((item)=>{
-    return item*2;
-})
+// for(let i =1; i<arr.length; i++){
+//     if(arr[i]<max){
+//         max = arr[i]
+//     }
+// }
 
-const newArray2  = array.forEach((itm)=> itm);
-console.log("newArray2",newArray2);
-const qw = Array.from(array);
-console.log("qw",typeof(qw));
+// const maxNum = Math.min(...arr);
+// console.log("maxNum",maxNum);
 
+// const array = [3,4,1,7,9];
+// const newArray = array.map((item)=>{
+//     return item*2;
+// })
 
-const nw = array.map((itm)=> itm);
-console.log("eee",typeof(nw));
-
-
-const ww =[{ key: 2 }];
-console.log("ww",ww);
-
-const w2 = array.concat("A: 21")
-const w21 = array.concat("AB: 210");
-
-const new1Data = [
-     [
-        '',
-        'about-us',
-        'our-brand',
-        'investors',
-        'careers#',
-        'sustainability',
-        'our-blog',
-        'contact-us'
-      ]
-]
+// const newArray2  = array.forEach((itm)=> itm);
+// console.log("newArray2",newArray2);
+// const qw = Array.from(array);
+// console.log("qw",typeof(qw));
 
 
+// const nw = array.map((itm)=> itm);
+// console.log("eee",typeof(nw));
 
+
+// const ww =[{ key: 2 }];
+// console.log("ww",ww);
+
+// const w2 = array.concat("A: 21")
+// const w21 = array.concat("AB: 210");
+
+// const new1Data = [
+//      [
+//         '',
+//         'about-us',
+//         'our-brand',
+//         'investors',
+//         'careers#',
+//         'sustainability',
+//         'our-blog',
+//         'contact-us'
+//       ]
+// ]
+
+const months = ["March", "Jan", "Feb", "Dec"];
+months.sort();
+console.log(months);
+// Expected output: Array ["Dec", "Feb", "Jan", "March"]
+
+const array1 = [1, 30, 4, 21, 100000];
+array1.sort();
+console.log(array1);
+// Expected output: Array [1, 100000, 21, 30, 4]
+
+
+
+
+const values = [1, 10, 21, 2];
+const sortedValues = values.toSorted((a, b) => a - b);
+
+const sortedArray = arrraynew.sort((a, b) => a - b);
+console.log(sortedArray);
+
+
+
+
+
+console.log(sortedValues);
 
 
 
